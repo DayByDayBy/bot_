22dd171f7ce6d1890208db170ea4c2c8cdbc3667
Diomedes Git <diomedesgit@gmail.com>
2025-05-27 17:54:05 +0100

    some bones

---
+++ api_etc/api_handler.py
@@ -0,0 +1,14 @@
+
+
+def post_reply(api, post_id, reply_text):
+    pass
+
+def get_posts(api):
+    pass
+
+
+
+
+# these will of course depend on platform/api specifics, but the basic idea is the same, ie grab some posts, post one reply
+# 
+# i may write a few functions, a pair per application
